cfe8f23d14ce326a0a91a16fb80c2225cf23aef3
unknown <driyant.mail@gmail.com>
2021-04-24 21:10:58 +0700

    Create tallying up methods

--- js/script.js
+++ js/script.js
@@ -1,16 +1,24 @@
 // Create politician obj
-const candidates = (candidate) => {
+const candidates = (candidateName) => {
     const politician = {
-        name : candidate,
+        name : candidateName,
         electionResults : null,
-        totalVotes : 0
+        totalVotes : 0,
+        tallyUpResults : function() {
+            let resultVotes = this.totalVotes;
+            const resultElections = this.electionResults
+            for(let result of resultElections) {
+                resultVotes = resultVotes + result;
+            }
+            return resultVotes;
+        }
     };
     return politician;
 }
 
 // Create 2 politicians
-const firstCandidate = candidates("Jubilee");
-const secondCandidate = candidates("Austeen");
+let firstCandidate = candidates("Jubilee");
+let secondCandidate = candidates("Austeen");
 
 // Add electionResult array
 firstCandidate.electionResults = [5, 1, 7, 2, 33, 6, 4, 2, 1, 14, 8, 3, 1, 11, 11, 0, 5, 3, 3, 3, 7, 4, 8, 9, 3, 7, 2, 2, 4, 2, 8, 3, 15, 15, 2, 12, 0, 4, 13, 1, 3, 2, 8, 21, 3, 2, 11, 1, 3, 7, 2];
@@ -23,3 +31,7 @@ firstCandidate.electionResults[4] = 17;
 secondCandidate.electionResults[4] = 38;
 firstCandidate.electionResults[43] = 11;
 secondCandidate.electionResults[43] = 27
+
+// Make Tally Up of electionResult
+firstCandidate.tallyUpResults();
+secondCandidate.tallyUpResults();

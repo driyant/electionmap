2a84f7ca31ef1c0b7c887d9437d8011239475498
unknown <driyant.mail@gmail.com>
2021-04-24 19:32:28 +0700

    Create two instances, and refactory code using factory function

--- js/script.js
+++ js/script.js
@@ -1,6 +1,15 @@
 // Create politician obj
-const politician = {
-    name : "",
-    electronResults : null,
-    totalVotes : 0
-};
+const candidates = (candidate) => {
+    const politician = {
+        name : candidate,
+        electronResults : null,
+        totalVotes : 0
+    };
+    return politician;
+}
+
+const getFirstCandidate = candidates("Jubilee");
+console.log(getFirstCandidate);
+const getSecondCandidate = candidates("Austeen");
+console.log(getSecondCandidate);
+
